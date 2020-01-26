00932d10d2b485b0ed9816c3267b73fa41333c26
wsites <60293983+wsites@users.noreply.github.com>
2020-01-26 16:06:54 -0500

    Add files via upload

---
+++ Week 1 - Jupyter Notebook.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[3]:
+
+
+a=1
+b=7
+a*b
+
+
+# In[7]:
+
+
+import pandas
+import numpy
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pylab as plt
+
+
+# In[8]:
+
+
+plt.plot ([3,5,7],[4,6,8])
+
+
+# In[13]:
+
+
+plt.scatter([1,2,6,9],[4,3,7,4])
+
